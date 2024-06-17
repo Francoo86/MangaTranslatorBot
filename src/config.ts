7e69444950cd181fb61d60bdb7671731ab7d3b18
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-17 17:14:10 -0400

    add basic setup

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,10 @@ dotenv.config();
 
 const {DISCORD_BOT_TOKEN, DISCORD_CLIENT_ID} = process.env;
 
+if(!DISCORD_BOT_TOKEN || !DISCORD_CLIENT_ID) {
+    throw new Error('Discord bot token and client ID must be provided in the environment variables DISCORD_BOT_TOKEN and DISCORD_CLIENT_ID');
+}
+
 export const config = {
   DISCORD_BOT_TOKEN,
   DISCORD_CLIENT_ID,
