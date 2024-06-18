da59795f0e953fd0da5e6adaa9342be98a077832
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-17 21:37:19 -0400

    add lang params

---
+++ src/translate_params/lang.ts
@@ -0,0 +1,24 @@
+const LANG_MAP: { [key: string]: string } = {
+    "简体中文": "CHS",
+    "繁體中文": "CHT",
+    "日本語": "JPN",
+    "English": "ENG",
+    "한국어": "KOR",
+    "Tiếng Việt": "VIN",
+    "čeština": "CSY",
+    "Nederlands": "NLD",
+    "français": "FRA",
+    "Deutsch": "DEU",
+    "magyar nyelv": "HUN",
+    "italiano": "ITA",
+    "polski": "PLK",
+    "português": "PTB",
+    "limba română": "ROM",
+    "русский язык": "RUS",
+    "español": "ESP",
+    "Türk dili": "TRK",
+    "Indonesia": "IND"
+};
+
+//export the lang map
+export { LANG_MAP };
