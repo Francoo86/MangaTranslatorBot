cb03c713a59f4bedb437e6e4eb9d5943ca650a82
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-17 20:58:51 -0400

    add ping command for testing

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,7 +2,12 @@ import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder()
   .setName("ping")
-  .setDescription("Replies with Pong!");
+  .setDescription("Replies with Pong!")
+  .addStringOption(option => 
+    option.setName('string')
+          .setDescription('Enter a string')
+          .setRequired(false)
+  );
 
 export async function execute(interaction: CommandInteraction) {
   return interaction.reply("Pong!");
