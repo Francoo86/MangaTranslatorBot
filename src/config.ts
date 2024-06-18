60fee3130586a7302e587e5debafd8e780f5f961
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-17 21:01:03 -0400

    check if api_url is present

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const {DISCORD_BOT_TOKEN, DISCORD_CLIENT_ID} = process.env;
+const {DISCORD_BOT_TOKEN, DISCORD_CLIENT_ID, API_URL} = process.env;
 
-if(!DISCORD_BOT_TOKEN || !DISCORD_CLIENT_ID) {
-    throw new Error('Discord bot token and client ID must be provided in the environment variables DISCORD_BOT_TOKEN and DISCORD_CLIENT_ID');
+if(!DISCORD_BOT_TOKEN || !DISCORD_CLIENT_ID || !API_URL) {
+    throw new Error('Discord bot token and client ID must be provided in the environment variables DISCORD_BOT_TOKEN and DISCORD_CLIENT_ID and API_URL');
 }
 
 export const config = {
-  DISCORD_BOT_TOKEN,
-  DISCORD_CLIENT_ID,
+    DISCORD_BOT_TOKEN,
+    DISCORD_CLIENT_ID,
+    API_URL
 };
