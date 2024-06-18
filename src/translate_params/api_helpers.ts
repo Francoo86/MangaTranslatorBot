72bcaf4d61cbead5ec62719cac1aa46fb5535c99
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-18 19:52:34 -0400

    remove hardcoding and validate image

--- src/translate_params/api_helpers.ts
+++ src/translate_params/api_helpers.ts
@@ -1,18 +1,41 @@
 import { TRANSLATORS } from "./translators";
 import { config } from "../config";
 import axios from "axios";
-import { CommandInteraction, EmbedBuilder, AttachmentBuilder } from "discord.js";
+import { CommandInteraction, EmbedBuilder, AttachmentBuilder, Attachment } from "discord.js";
 import { LANG_MAP } from "./lang";
 
 const API_URL = config.API_URL;
 const RESULT_ENDPOINT = "/result";
+
+const ARONA_COLOR = 0x33ADFF;
+
+function isValidURL(url : string) {
+    try {
+        new URL(url);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
 
 function getNameBasedOnValue(value : string, map : { value: string, name: string }[]) : string {
     const item = map.find(t => t.value === value);
     return item ? item.name : "Unknown";
 }
 
+function tryToGetImageUrl(interaction: CommandInteraction): string | null {
+  const panel = interaction.options.get('panel')?.attachment as Attachment;
+  if (panel) {
+      return panel.url;
+  }
 
+  const panel_url = interaction.options.get('panel_url')?.value as string;
+  if (panel_url && isValidURL(panel_url)) {
+      return panel_url.split(" ")[0];
+  }
+
+  return null;
+}
 
 function getLanguageName(value : string) : string {
     return getNameBasedOnValue(value, LANG_MAP);
@@ -38,7 +61,7 @@ async function loadResult(interaction : CommandInteraction, taskId : string) {
       const translatorName = getTranslatorName(translator);
   
       const embed = new EmbedBuilder()
-      .setColor(0x33ADFF)
+      .setColor(ARONA_COLOR)
         .setTitle("AronaTranslator")
         .setDescription("Translation complete! Check out the translated panel below.")
         .addFields(
@@ -62,4 +85,4 @@ async function loadResult(interaction : CommandInteraction, taskId : string) {
     }
   }
 
-export { loadResult}
+export { loadResult, tryToGetImageUrl}
