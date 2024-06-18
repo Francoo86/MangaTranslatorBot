8f1da4a9462d4efe92f3d23c65b26ef6038e0390
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-17 21:42:46 -0400

    change to interface the lang thingy

--- src/translate_params/lang.ts
+++ src/translate_params/lang.ts
@@ -1,24 +1,29 @@
-const LANG_MAP: { [key: string]: string } = {
-    "简体中文": "CHS",
-    "繁體中文": "CHT",
-    "日本語": "JPN",
-    "English": "ENG",
-    "한국어": "KOR",
-    "Tiếng Việt": "VIN",
-    "čeština": "CSY",
-    "Nederlands": "NLD",
-    "français": "FRA",
-    "Deutsch": "DEU",
-    "magyar nyelv": "HUN",
-    "italiano": "ITA",
-    "polski": "PLK",
-    "português": "PTB",
-    "limba română": "ROM",
-    "русский язык": "RUS",
-    "español": "ESP",
-    "Türk dili": "TRK",
-    "Indonesia": "IND"
-};
+interface Language {
+    name: string;
+    value: string;
+}
 
-//export the lang map
+const LANG_MAP: Language[] = [
+    { name: "简体中文", value: "CHS" },
+    { name: "繁體中文", value: "CHT" },
+    { name: "日本語", value: "JPN" },
+    { name: "English", value: "ENG" },
+    { name: "한국어", value: "KOR" },
+    { name: "Tiếng Việt", value: "VIN" },
+    { name: "čeština", value: "CSY" },
+    { name: "Nederlands", value: "NLD" },
+    { name: "français", value: "FRA" },
+    { name: "Deutsch", value: "DEU" },
+    { name: "magyar nyelv", value: "HUN" },
+    { name: "italiano", value: "ITA" },
+    { name: "polski", value: "PLK" },
+    { name: "português", value: "PTB" },
+    { name: "limba română", value: "ROM" },
+    { name: "русский язык", value: "RUS" },
+    { name: "español", value: "ESP" },
+    { name: "Türk dili", value: "TRK" },
+    { name: "Indonesia", value: "IND" }
+];
+
+// Export the LANG_MAP
 export { LANG_MAP };
