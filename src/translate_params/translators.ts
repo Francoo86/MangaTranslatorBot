bd751b885014e2c1ad7716b60796a7ab2aa2b33d
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-06-18 20:00:02 -0400

    remove gpt3.5 because gives rate limiting

--- src/translate_params/translators.ts
+++ src/translate_params/translators.ts
@@ -3,9 +3,10 @@ import { Translator } from "./interfaces";
 const TRANSLATORS : Translator[] = [
     {name: "Google Translate", value: "google"},
     {name: "DeepL", value: "deepl"},
-    {name: "GPT-3.5", value: "gpt3.5"},
+    //{name: "GPT-3.5", value: "gpt3.5"},
     {name: "M2M-100", value: "m2m100"},
-    {name: "Sugoi", value: "sugoi"},
+    {name: "M2M-100 Big", value: "m2m100_big"},
+    {name: "Sugoi (only English supported)", value: "sugoi"},
     //maybe i will add bing
 ]
 
